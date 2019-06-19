573265ef6297673ea4afdbde65f89f1e77e425ff
tony <chcjswo@gmail.com>
2019-06-19 23:43:38 +0900

    Project Init

---
+++ src/main/java/board/board/controller/HelloController.java
@@ -0,0 +1,14 @@
+package board.board.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String greet() {
+        return "Hello World~~";
+    }
+}
